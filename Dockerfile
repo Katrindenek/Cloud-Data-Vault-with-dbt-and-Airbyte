cf671abff03a15cc5ba68b03e1f089360fe9ffd4
Katrindenek <zapkokaty@gmail.com>
2024-05-31 21:51:13 +0300

    Expand Terraform configuration

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+ARG DBT_VERSION=1.0.0
+FROM fishtownanalytics/dbt:${DBT_VERSION}
+
+
+# Install utilities, yc CLI, and Terraform
+RUN apt -y update && apt -y upgrade && \
+    apt -y install curl wget gpg unzip && \
+    # Install yc CLI
+    curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash -s -- -a && \
+    # Install Terraform
+    curl -sL https://hashicorp-releases.yandexcloud.net/terraform/1.4.6/terraform_1.4.6_linux_amd64.zip -o terraform.zip && \
+    unzip terraform.zip && \
+    install -o root -g root -m 0755 terraform /usr/local/bin/terraform && \
+    rm -rf terraform terraform.zip && \
+    # Clean up
+    apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install dbt adapter
+RUN set -ex && \
+    python -m pip install --upgrade pip setuptools && \
+    python -m pip install dbt-clickhouse==1.4.0 dbt-core==1.4.6
+
+# Define environment variables
+ENV DBT_PROFILES_DIR=/usr/app/.dbt \
+    PATH=$PATH:/root/yandex-cloud/bin
+
+# Set work directory
+WORKDIR /usr/app/
+
+# Keep container running
+ENTRYPOINT ["tail", "-f", "/dev/null"]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,31 @@
-variable "yandex_cloud_token" {
-  description = "Yandex Cloud OAuth token"
+variable "folder_id" {
+  description = "Folder ID"
+  type        = string
 }
 
-variable "yandex_cloud_id" {
-  description = "Yandex Cloud ID"
+variable "clickhouse_password" {
+  description = "Clickhouse admin password"
+  type        = string
+  sensitive   = true
 }
 
-variable "yandex_folder_id" {
-  description = "Yandex Folder ID"
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
+}
+
+output "clickhouse_host_fqdn" {
+  value = resource.yandex_mdb_clickhouse_cluster.clickhouse_starschema.host[0].fqdn
+}
+
+output "yandex_iam_service_account_static_access_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+}
+output "yandex_iam_service_account_static_secret_key" {
+  value     = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  sensitive = true
+}
+
+output "yandex_compute_instance_nat_ip_address" {
+  value = yandex_compute_instance.airbyte.network_interface.0.nat_ip_address
 }
